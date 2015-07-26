31a26ea56b76df3939fb9fab5e37fd1423f9eeb2
Ray Walker <hello@raywalker.it>
2015-07-26 15:58:18 +1000

    pagespeed https mapping

--- nginx-pagespeed/boot.sh
+++ nginx-pagespeed/boot.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ ! -f /etc/nginx/ssl/dhparam.pem ] ; then
-	echo " * nginx:  generating /etc/nginx/ssl/dhparam.pem ..."
-	mkdir -p /etc/nginx/ssl
-	openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
-fi
-
 echo -e "\nDone\n$(date)\n"
 
 if [[ "$1" = "/sbin/my_init" ]] ; then
--- nginx-pagespeed/init_nginx.sh
+++ nginx-pagespeed/init_nginx.sh
@@ -2,11 +2,11 @@
 set -e
 
 mkdir -p /etc/nginx/ssl
+mkdir -p /app/www
+
+mv /index.html /app/www
 
 # Generate a self signed certificate
 if [ ! -f /etc/nginx/ssl/default.key ] ; then
 	openssl req -x509 -newkey rsa:2048 -keyout /etc/nginx/ssl/default.key -out /etc/nginx/ssl/default.crt -nodes -days 365 -subj "/C=AU/ST=Tas/L=Launceston/O=/OU=/CN=localdomain"
 fi
-
-
-echo -e "funkygibbon/nginx\n`date`" > /app/www/index.html
