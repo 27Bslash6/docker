d85d6dee0accb1614d5d5a47790c475e98c3f7e8
Ray Walker <hello@raywalker.it>
2015-10-19 00:46:55 +1100

    Add simple build script

---
+++ build_nginx_suite.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+NAMESPACE=funkygibbon
+
+PROJECT_DIR=`pwd`
+
+PROJECTS=("nginx" "nginx-pagespeed" "nginx-php-exim" "nginx-proxy" )
+
+cd $PROJECT_DIR/nginx
+
+for i in "${PROJECTS[@]}"
+do
+  cd $PROJECT_DIR/$i
+  echo -e "Building $NAMESPACE/$i ..."
+  docker build -t $NAMESPACE/$i .
+
+done
