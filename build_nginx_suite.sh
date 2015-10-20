0e9d0a498662639279c7a7dbd00f1628a12ee004
Ray Walker <hello@raywalker.it>
2015-10-20 12:20:16 +1100

    wrap shell vars in {}

--- build_nginx_suite.sh
+++ build_nginx_suite.sh
@@ -8,12 +8,12 @@ PROJECT_DIR=`pwd`
 
 PROJECTS=("nginx" "nginx-pagespeed" "nginx-php-exim" "nginx-proxy" )
 
-cd $PROJECT_DIR/nginx
+cd ${PROJECT_DIR}/nginx
 
 for i in "${PROJECTS[@]}"
 do
-  cd $PROJECT_DIR/$i
-  echo -e "Building $NAMESPACE/$i ..."
-  docker build -t $NAMESPACE/$i .
+  cd ${PROJECT_DIR}/${i}
+  echo -e "Building ${NAMESPACE}/${i} ..."
+  docker build -t ${NAMESPACE}/${i} .
 
 done
