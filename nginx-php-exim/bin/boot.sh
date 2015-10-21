d9a3c81c70d13c0753c33f204124759ce32aa111
Ray Walker <hello@raywalker.it>
2015-10-21 18:52:47 +1100

    Force delete backup exim template on boot

--- nginx-php-exim/bin/boot.sh
+++ nginx-php-exim/bin/boot.sh
@@ -87,6 +87,7 @@ else
 fi
 
 echo " * exim4:  restarting service ..."
+rm -f /etc/exim4/exim4.conf.template.bak*
 service exim4 restart >> /dev/null
 
 # -----------------------------------------------------------------------------
