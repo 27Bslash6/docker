c65b9fd6293f338d80c7f241ab8feab16b0c949f
Ray Walker <hello@raywalker.it>
2016-01-30 15:15:56 +1100

    readability changes

--- nginx-php-exim/bin/init_php.sh
+++ nginx-php-exim/bin/init_php.sh
@@ -10,6 +10,7 @@ echo "export TERM=xterm-256color" >> /root/.bashrc
 
 # PHP-FPM
 
+# Backup original information
 cp /etc/php5/fpm/php.ini /etc/php5/fpm/php.ini.dist
 cp /etc/php5/fpm/pool.d/www.conf /etc/php5/fpm/pool.d/www.conf.dist
 
@@ -31,19 +32,22 @@ echo "xdebug.var_display_max_children=256" >> /etc/php5/mods-available/xdebug.in
 
 # Still necessary in case of misconfiguration in sites-enabled/
 sed -i -r "s/;cgi.fix_pathinfo\s*=\s*1/cgi.fix_pathinfo=0/g" /etc/php5/fpm/php.ini
+
 # Don't fork
 sed -i -r "s/;daemonize = yes/daemonize = no/g" /etc/php5/fpm/php-fpm.conf
+
 # Catch PHP output
 sed -i -r "s/;catch_workers_output =/catch_workers_output =/g" /etc/php5/fpm/pool.d/www.conf
+
 # Fix socket permissions
 sed -i -r "s/;listen.mode = 0660/listen.mode = 0750/g" /etc/php5/fpm/pool.d/www.conf
+
 # Enable sendmail additional parameters
 sed -i -r "s/;sendmail_path =/sendmail_path =/g" /etc/php5/fpm/php.ini
 
+# Clear upstream data
 rm /app/www/index.html
 echo "<?php phpinfo(); " > /app/www/index.php
 
-# EXIM
-
-# enable TLS
+# enable Exim TLS
 echo "MAIN_TLS_ENABLE = 1" >> /etc/exim4/exim4.conf.localmacros
