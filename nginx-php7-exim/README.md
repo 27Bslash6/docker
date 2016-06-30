b92d09ad863601579f870ae5c753cedf8975d68f
Joost van der Laan <joostvanderlaan@gmail.com>
2016-06-30 22:58:17 +0200

    WIP php7

---
+++ nginx-php7-exim/README.md
@@ -0,0 +1,40 @@
+# Nginx + PHP5-FPM + Exim
+
+Highly configurable nginx-PHP webserver stack built on [funkygibbon/nginx-pagespeed](https://hub.docker.com/r/funkygibbon/nginx-pagespeed/), which is in turn built on a [lightly modified Phusion Ubuntu base image](https://hub.docker.com/r/funkygibbon/ubuntu/)
+
+Docker Hub: [funkygibbon/nginx-php-exim](https://hub.docker.com/r/funkygibbon/nginx-php-exim/)
+
+`docker run -p "80:80" -p "443:443" -e "APP_HOSTNAME=some.example.com" -v /some/dir/www:/app/www funkygibbon/nginx-php-exim`
+
+Configurable via a plethora of environment variables, which are applied on service start
+
+var | default | description
+--- | ------- | -----------
+APP_ENV | production | production, development :: 'development' enables http://www.xdebug.org/
+ADMIN_EMAIL | nobody@example.com | Server administrator email, used for intercepted email in `development` mode
+CHOWN_APP_DIR | true | if true, `chown -R $APP_USER:$APP_GROUP /app/www`
+APP_HOSTNAME | `hostname -f` |  hostname of application 
+VIRTUAL_HOST | example.com | virtualhosts which this service should respond to, separated by commmas.  Useful for operating behind [jwilder/nginx-proxy](https://hub.docker.com/r/jwilder/nginx-proxy/).
+TIMEZONE | Australia/Sydney | Server timezone
+APP_USER | app | nginx and php5-fpm user 
+APP_GROUP | app | nginx and php5-fpm group
+APP_UID | 1000 | user_id - useful when mounting volumes from host > guest to either share or delineate file access permission
+APP_GID | 1000 | group_id
+UPLOAD_MAX_SIZE | 30M | Maximum upload size, applied to nginx and php5-fpm
+NGINX_MAX_WORKER_PROCESSES | 8 | nginx worker_processes is determined from number of processor cores on service start, up to the maximum permitted by NGINX_MAX_WORKER_PROCESSES
+PHP_MEMORY_LIMIT | 128M | Maximum memory PHP can use per worker
+PHP_PROCESS_MANAGER | dynamic | dynamic, static, ondemand :: PHP process manager scheme
+PHP_MAX_CHILDREN | 6 | process manager maximum spawned children 
+PHP_START_SERVERS | 3 | if PHP_PROCESS_MANAGER is dynamic, this is the number of children spawned on boot
+PHP_MIN_SPARE_SERVERS | 2 | if PHP_PROCESS_MANAGER is dynamic, this is the minimum number of idle children 
+PHP_MAX_SPARE_SERVERS | 3 | if PHP_PROCESS_MANAGER is dynamic, this is the maximum number of idle children
+PHP_MAX_REQUESTS | 500 | Maximum number of requests each child process can process before terminating, which should mitigate any memory leaks. Set to 0 to disable.
+PHP_DISABLE_FUNCTIONS | false | Comma separated list of additional functions to disable for security.  These are appended to the default Ubuntu distribution disable_functions line 
+PHP_XDEBUG_REMOTE_HOST | 172.17.42.1 | If $APP_ENV is `development`, XDebug is enabled and configured to communicate to this remote host
+PHP_XDEBUG_REMOTE_PORT | 9000 | XDebug port
+PHP_XDEBUG_IDE_KEY | default_ide_key | XDebug IDE Key
+EXIM_DELIVERY_MODE | local | smarthost, local :: set to smarthost to enable third party SMTP
+EXIM_MAIL_FROM | example.com | domain from which exim4 mail appears to originate
+EXIM_SMARTHOST | smtp.example.org::587 | smarthost relay SMTP server address and port (note the double colon (::) before port number)
+EXIM_SMARTHOST_AUTH_USERNAME | postmaster@example.com | SMTP username
+EXIM_SMARTHOST_AUTH_PASSWORD | password_123 | SMTP password
---
+++ nginx-php7-exim/bin/boot.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+#
+# boot.sh
+# Applies docker-compose environment variables to applications on container start
+# Restarts appropriate services (only exim4 at this point)
+
+set -e
+
+echo "Starting config in ${APP_ENV:-$DEFAULT_APP_ENV} mode ..."
+
+export APP_HOSTNAME=${APP_HOSTNAME:-`hostname -f`}
+
+echo " * host:   ${APP_HOSTNAME}"
+
+# =============================================================================
+# 	EXIM4
+# =============================================================================
+
+if [[ "${EXIM_DELIVERY_MODE:-$DEFAULT_EXIM_DELIVERY_MODE}" = "smarthost" ]] ; then	
+
+	# -------------------------------------------------------------------------
+	# 	SMARTHOST
+	# -------------------------------------------------------------------------
+	
+	echo " * exim4:  smarthost"
+	
+	if [[ "${EXIM_SMARTHOST_AUTH_USERNAME:-$DEFAULT_EXIM_SMARTHOST_AUTH_USERNAME}" = "postmaster@example.com" ]] ; then
+
+		echo "ERROR:   exim4:  Your username is 'postmaster@example.com'"
+		echo " *                Please rebuild using EXIM_SMARTHOST_AUTH_USERNAME and EXIM_SMARTHOST_AUTH_PASSWORD"
+		echo " *                $ docker run -e \"EXIM_SMARTHOST_AUTH_USERNAME=foo\" etc"
+
+	else
+
+		echo " * exim4:  *:${EXIM_SMARTHOST_AUTH_USERNAME:-$DEFAULT_EXIM_SMARTHOST_AUTH_USERNAME}:********"
+
+		echo "*:${EXIM_SMARTHOST_AUTH_USERNAME:-$DEFAULT_EXIM_SMARTHOST_AUTH_USERNAME}:${EXIM_SMARTHOST_AUTH_PASSWORD:-$DEFAULT_EXIM_SMARTHOST_AUTH_PASSWORD}" > /etc/exim4/passwd.client
+		# Configure exim4 mta smarthost to use sendgrid or mailgun
+		sed -i -r "s/dc_eximconfig_configtype='[a-z]*'/dc_eximconfig_configtype='smarthost'/" /etc/exim4/update-exim4.conf.conf
+		
+		echo " * exim4:  relay: ${EXIM_SMARTHOST:-$DEFAULT_EXIM_SMARTHOST}"
+		sed -i -r "s/dc_smarthost=.*/dc_smarthost='${EXIM_SMARTHOST:-$DEFAULT_EXIM_SMARTHOST}'/" /etc/exim4/update-exim4.conf.conf
+				
+		echo " * exim4:  from:  ${EXIM_MAIL_FROM:-$DEFAULT_EXIM_MAIL_FROM}"
+		sed -i -r "s/dc_readhost='.*'/dc_readhost='${EXIM_MAIL_FROM:-$DEFAULT_EXIM_MAIL_FROM}'/" /etc/exim4/update-exim4.conf.conf
+		
+
+		sed -i -r "s/dc_hide_mailname=''/dc_hide_mailname='true'/" /etc/exim4/update-exim4.conf.conf
+
+	fi
+
+	
+else
+
+	# -------------------------------------------------------------------------
+	# 	LOCAL
+	# -------------------------------------------------------------------------
+	
+	echo " * exim4:  local"
+
+	sed -i -r "s/dc_eximconfig_configtype=.*/dc_eximconfig_configtype='local'/g" /etc/exim4/update-exim4.conf.conf
+	sed -i -r "s/dc_smarthost=.*/dc_smarthost=''/g" /etc/exim4/update-exim4.conf.conf
+	sed -i -r "s/dc_hide_mailname=.*/dc_hide_mailname='true'/g" /etc/exim4/update-exim4.conf.conf
+
+	echo " * exim4:  rm /etc/exim4/passwd.client"
+	rm -f /etc/exim4/passwd.client
+
+fi
+
+# -----------------------------------------------------------------------------
+#   EXIM email redirect
+# -----------------------------------------------------------------------------
+
+if [[ "${APP_ENV:-$DEFAULT_APP_ENV}" = "development" ]]; then
+
+	if [[ ${ADMIN_EMAIL:-$DEFAULT_ADMIN_EMAIL} = "nobody@example.com" ]]; then
+		echo " ** WARNING ** admin email is default - nobody@example.com - emails will disappear into the void"
+	fi
+
+	echo " * exim4:  re-routing all outgoing email to ${ADMIN_EMAIL:-$DEFAULT_ADMIN_EMAIL}"
+	echo -e "catch_all_outgoing:\n  debug_print = \"R: redirecting email for \$local_part@\$domain\"\n  driver = redirect\n  domains = ! +local_domains\n  allow_fail\n  data = ${ADMIN_EMAIL:-$DEFAULT_ADMIN_EMAIL}\n  no_more\n" > /etc/exim4/conf.d/router/190_exim4-config_intercept
+	
+	update-exim4.conf.template -r
+
+else
+	rm -fr /etc/exim4/conf.d/router/190_exim4-config_intercept
+fi
+
+echo " * exim4:  restarting service ..."
+rm -f /etc/exim4/exim4.conf.template.bak*
+service exim4 restart >> /dev/null
+
+# -----------------------------------------------------------------------------
+
+
+# =============================================================================
+# 	BOOT
+# =============================================================================
+
+echo -e "\nDone\n$(date)\n"
+
+if [[ "$1" = "/sbin/my_init" ]] ; then
+	exec /sbin/my_init 
+else
+	echo "$ /bin/sh -c $@"
+	exec /bin/sh -c "$@"
+fi
