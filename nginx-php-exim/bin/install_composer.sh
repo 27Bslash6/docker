21ebe66f62657a9ac3c001faec3b963af8f6dd25
Ray Walker <hello@raywalker.it>
2017-09-07 14:13:25 +0200

    fix for occasional crypto failures

--- nginx-php-exim/bin/install_composer.sh
+++ nginx-php-exim/bin/install_composer.sh
@@ -3,7 +3,7 @@
 # https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
 
 EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "copy('http://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
