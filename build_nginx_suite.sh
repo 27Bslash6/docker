9f5f553a2fc570571827de653bde42cf5dc3408f
Ray Walker <hello@raywalker.it>
2017-11-07 21:14:45 +1100

    Version bump. - nginx v1.13.6 - openssl 1.1.0g - headers_more 0.33

--- build_nginx_suite.sh
+++ build_nginx_suite.sh
@@ -8,17 +8,17 @@ SOURCE_NAMESPACE=${BUILD_SOURCE_NAMESPACE:-"funkygibbon"}
 TAG=${BUILD_TAG:-"latest"}
 
 # http://nginx.org/en/download.html
-NGINX_VERSION=${BUILD_NGINX_VERSION:-"1.13.3"}
+NGINX_VERSION=${BUILD_NGINX_VERSION:-"1.13.6"}
 
 # https://github.com/pagespeed/ngx_pagespeed/releases
 NGINX_PAGESPEED_VERSION=${BUILD_PAGESPEED_VERSION:-"latest"}
 NGINX_PAGESPEED_RELEASE_STATUS=${BUILD_PAGESPEED_RELEASE_STATUS:-"stable"}
 
 # https://www.openssl.org/source
-OPENSSL_VERSION=${BUILD_OPENSSL_VERSION:-"1.1.0f"}
+OPENSSL_VERSION=${BUILD_OPENSSL_VERSION:-"1.1.0g"}
 
 # https://github.com/openresty/headers-more-nginx-module/tags
-HEADERS_MORE_VERSION=${BUILD_OPENSSL_VERSION:-"0.32"}
+HEADERS_MORE_VERSION=${BUILD_OPENSSL_VERSION:-"0.33"}
 
 PHP_VERSION=${BUILD_OPENSSL_VERSION:-"7.0"}
 
