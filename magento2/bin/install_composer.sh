897afff69830d2e550d7e104be36c200a126c4f4
Ray Walker <hello@raywalker.it>
2017-11-07 21:19:10 +1100

    Inherit changes from greenpeace/planet4-docker

--- magento2/bin/install_composer.sh
+++ magento2/bin/install_composer.sh
@@ -1,17 +1,33 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -e
 
-EXPECTED_SIGNATURE=$(wget https://composer.github.io/installer.sig -O - -q)
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+# Description: Install composer from latest source
+# Author: Raymond Walker <raymond.walker@greenpeace.org>
+# https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+# Modified from original script to:
+#  - fail on any script errors
+#  - retry connection failures using wget
+#  - install hirak/prestissimo (parallel composer dependency downloader)
+
+retries=5
+
+EXPECTED_SIGNATURE=$(wget --retry-connrefused --waitretry=1 -t $retries -q -O - https://composer.github.io/installer.sig)
+
+wget --retry-connrefused --waitretry=1 -t $retries -O composer-setup.php http://getcomposer.org/installer
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
-if [ "$EXPECTED_SIGNATURE" = "$ACTUAL_SIGNATURE" ]
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
 then
-    php composer-setup.php
-    RESULT=$?
-    rm composer-setup.php
-    exit $RESULT
-else
     >&2 echo 'ERROR: Invalid installer signature'
     rm composer-setup.php
     exit 1
 fi
+
+# Installs composer.phar to /app/bin/composer.phar
+# Available on path, wrapped by /app/bin/composer.sh
+php composer-setup.php --install-dir=/app/bin
+rm composer-setup.php
+
+# Install parallel downloader hirak/prestissimo (latest version)
+echo "Installing hirak/prestissimo ..."
+/app/bin/composer.phar --no-plugins --no-scripts --profile -vvv global require hirak/prestissimo
--- nginx-php-exim/bin/install_composer.sh
+++ nginx-php-exim/bin/install_composer.sh
@@ -1,9 +1,19 @@
-#!/bin/sh
-# Find updated scripts at:
-# https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+#!/usr/bin/env bash
+set -e
 
-EXPECTED_SIGNATURE=$(wget -q -O - https://composer.github.io/installer.sig)
-php -r "copy('http://getcomposer.org/installer', 'composer-setup.php');"
+# Description: Install composer from latest source
+# Author: Raymond Walker <raymond.walker@greenpeace.org>
+# https://getcomposer.org/doc/faqs/how-to-install-composer-programmatically.md
+# Modified from original script to:
+#  - fail on any script errors
+#  - retry connection failures using wget
+#  - install hirak/prestissimo (parallel composer dependency downloader)
+
+retries=5
+
+EXPECTED_SIGNATURE=$(wget --retry-connrefused --waitretry=1 -t $retries -q -O - https://composer.github.io/installer.sig)
+
+wget --retry-connrefused --waitretry=1 -t $retries -O composer-setup.php http://getcomposer.org/installer
 ACTUAL_SIGNATURE=$(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
@@ -13,8 +23,11 @@ then
     exit 1
 fi
 
-# php composer-setup.php --quiet
+# Installs composer.phar to /app/bin/composer.phar
+# Available on path, wrapped by /app/bin/composer.sh
 php composer-setup.php --install-dir=/app/bin
-RESULT=$?
 rm composer-setup.php
-exit $RESULT
+
+# Install parallel downloader hirak/prestissimo (latest version)
+echo "Installing hirak/prestissimo ..."
+/app/bin/composer.phar --no-plugins --no-scripts --profile -vvv global require hirak/prestissimo
