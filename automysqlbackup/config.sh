0aa6e16707972b10dc2f268ed23a09e6213af513
Ray Walker <hello@raywalker.it>
2022-07-22 13:55:36 +1000

    add cron schedule, for reals

--- automysqlbackup/config.sh
+++ automysqlbackup/config.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 AUTOMYSQLBACKUP_CONFIG_FILE=/etc/default/automysqlbackup
 MYSQL_CONFIG_FILE=/etc/mysql/debian.cnf
 
 sed -i -r "s/BACKUPDIR=.*/BACKUPDIR=\"\/backup\"/g" $AUTOMYSQLBACKUP_CONFIG_FILE
 
-echo " * host:      ${BACKUP_HOST}"
-sed -i -r "s/DBHOST=localhost/DBHOST=${BACKUP_HOST}/g" $AUTOMYSQLBACKUP_CONFIG_FILE
+_title "Configuring AutoMySQLBackup ..."
+_good "host:      ${DBHOST}"
+sed -i -r "s/DBHOST=localhost/DBHOST=${DBHOST}/g" $AUTOMYSQLBACKUP_CONFIG_FILE
 
-echo " * databases: ${BACKUP_DATABASES//,/ }"
-if [[ ${BACKUP_DATABASES} = "all" ]]; then
+_good "username:  ${USERNAME}"
+echo -e "[client]\nuser = ${USERNAME}\npassword = ${PASSWORD}\nhost = ${DBHOST}\nport = 3306" > $MYSQL_CONFIG_FILE
+
+_good "databases: ${DATABASES//,/ }"
+if [[ ${DATABASES} = "all" ]]; then
     sed -i -r "s/^DBNAMES=.*/DBNAMES=`mysql --defaults-file=/etc/mysql/debian.cnf --execute=\"SHOW DATABASES\" | awk '{print $1}' | grep -v ^Database$ | grep -v ^mysql$ | grep -v ^performance_schema$ | grep -v ^information_schema$ | tr \\\r\\\n ,\ `/g" $AUTOMYSQLBACKUP_CONFIG_FILE
 else
-    sed -i -r "s/^DBNAMES=.*/DBNAMES=\"${BACKUP_DATABASES//,/ }\"/g" $AUTOMYSQLBACKUP_CONFIG_FILE
+    sed -i -r "s/^DBNAMES=.*/DBNAMES=\"${DATABASES//,/ }\"/g" $AUTOMYSQLBACKUP_CONFIG_FILE
 fi
 
-echo " * username:  ${BACKUP_USERNAME}"
-echo -e "[client]\nuser = ${BACKUP_USERNAME}\npassword = ${BACKUP_PASSWORD}\nhost = ${BACKUP_HOST}\nport = 3306" > $MYSQL_CONFIG_FILE
+_good "schedule:  ${CRON_SCHEDULE}"
+(crontab -u root -l 2>/dev/null || true; echo "${CRON_SCHEDULE} automysqlbackup.sh" ) | crontab -u root -
 
-
-echo " * email:     ${BACKUP_EMAIL}"
-sed -i -r "s/MAILADDR=.*/MAILADDR=${BACKUP_EMAIL}/g" $AUTOMYSQLBACKUP_CONFIG_FILE
-
-
-
+if [[ -n "${EMAIL_TO:-}" ]]; then
+  _good "email:     ${EMAIL_TO}"
+  sed -i -r "s/MAILADDR=.*/MAILADDR=${EMAIL_TO}/g" $AUTOMYSQLBACKUP_CONFIG_FILE
+else
+  _warning "EMAIL_TO not set, will not send emails"
+fi
